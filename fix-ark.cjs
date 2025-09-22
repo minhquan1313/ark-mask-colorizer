18c7e13fab606f9f53069b63bc5f8122ad5564d3
minhquan1313 <minhquan1313@gmail.com>
2025-09-22 15:21:00 +0700

    add script to generate creatures.json

--- fix-ark.cjs
+++
@@ -1,8 +0,0 @@
-// fix-ark.js
-const fs = require('fs');
-const p = 'src/utils/arkPalette.js';
-let s = fs.readFileSync(p, 'utf8');
-// thay các ký tự backslash + n thành newline thực
-s = s.replace(/\\n/g, '\n');
-fs.writeFileSync(p, s, 'utf8');
-console.log('Fixed', p);
