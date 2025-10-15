83cd8a1b0c9af91682c10c6df11e27dcabff726d
minhquan1313 <minhquan1313@gmail.com>
2025-10-15 14:19:06 +0700

    add pic for structure

--- src/data/structureTypes.ts
+++ src/data/structureTypes.ts
@@ -2,6 +2,7 @@ export interface IArkStructure {
   id: string;
   name: string;
   decaySeconds: number;
+  image: string;
 }
 
 const DAY = 24 * 60 * 60;
@@ -11,31 +12,37 @@ export const STRUCTURE_TYPES: IArkStructure[] = [
     id: 'thatch',
     name: 'Thatch',
     decaySeconds: 4 * DAY,
+    image: '/assets/structures/thatch.svg',
   },
   {
     id: 'wood',
     name: 'Wood',
     decaySeconds: 8 * DAY,
+    image: '/assets/structures/wood.svg',
   },
   {
     id: 'adobe',
     name: 'Adobe',
     decaySeconds: 8 * DAY,
+    image: '/assets/structures/adobe.svg',
   },
   {
     id: 'stone',
     name: 'Stone',
     decaySeconds: 12 * DAY,
+    image: '/assets/structures/stone.svg',
   },
   {
     id: 'metal',
     name: 'Metal',
     decaySeconds: 16 * DAY,
+    image: '/assets/structures/metal.svg',
   },
   {
     id: 'tek',
     name: 'Tek',
     decaySeconds: 20 * DAY,
+    image: '/assets/structures/tek.svg',
   },
 ];
 
